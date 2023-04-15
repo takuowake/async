36d7132a0c24a007a7357fcd85e302b0af840b76
takuowake <takuowake@gmail.com>
2023-04-15 23:20:01 +0900

    Streamの実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+final streamController = StreamController<String>();
+
 void main() {
+  ///Future
   task1();
   task2();
   task3();
+  ///Stream
+  streamController.stream.listen((addData) {
+    debugPrint(addData);
+  });
+
+  //データを追加する
+  streamController.sink.add('赤');
+  streamController.sink.add('青');
+  streamController.sink.add('黄');
 }
 
 void task1() => debugPrint('task1 complete');
